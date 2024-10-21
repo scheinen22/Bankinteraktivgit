1991bcbe0766d32c85a8018b14a3bebb3bbd5375
scheinen22 <saidh.@MacBook-Pro-von-Said.local>
2024-10-21 15:31:53 +0200

    Tests hinzugefügt

--- src/test/java/banksystemtest/BankTest.java
+++ src/test/java/banksystemtest/BankTest.java
@@ -6,11 +6,12 @@ import banksystem.Kunde;
 import org.junit.jupiter.api.Test;
 
 import static banksystem.Bank.transfer;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertThat;
 
 class BankTest {
 
-    Bank bank = new Bank();
     Konto konto = new Konto(new Bank(12345, "Sparkasse"), 432323, 28923.12, -2000, 2000, new Kunde("Jochen", "Schmidt", "21.12.2000", "Teststraße", 1076.88));
     Konto konto2 = new Konto(new Bank(123456, "Sparkasse"), 2131, 900, -2000, 2000, new Kunde("Kai", "Humboldt", "12.10.2000", "Bevingsweg", 0));
     Konto konto3 = new Konto();
@@ -21,7 +22,8 @@ class BankTest {
         assertThrows(NullPointerException.class, () -> transfer(konto3, konto4, 2000, 123, 123));
     }
     @Test
-    void transaktionKontoIbanBLZFalsch() {
-        transfer(konto)
+    void transaktionKontoIbanBLZFalsch() throws Exception {
+        Bank.transfer(konto, konto2, 2000, 123, 123);
+        assertEquals(900, konto2.getKontostand());
     }
 }
